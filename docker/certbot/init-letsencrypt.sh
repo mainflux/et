8df67f69678e30efba9c2e9e65f0748e181473b3
SammyOina <sammyoina@gmail.com>
2023-05-30 14:00:03 +0300

    update for deployment
    Signed-off-by: SammyOina <sammyoina@gmail.com>

--- docker/certbot/init-letsencrypt.sh
+++ docker/certbot/init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker compose)" ]; then
   exit 1
 fi
 
-domains=(callhome.mainflux.com www.callhome.mainflux.com)
+domains=(deployments.mainflux.io www.deployments.mainflux.io)
 rsa_key_size=4096
 data_path="./certbot"
 email="infos@mainflux.com"
--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiKey: '77e04a7c-f207-40dd-8950-c344871fd516',
-  callHomeBaseUrl: "http://64.226.105.108"
+  callHomeBaseUrl: "https://deployments.mainflux.io"
 };
   
