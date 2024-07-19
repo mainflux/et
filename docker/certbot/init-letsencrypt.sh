50eacd7406069430b0b1d607e03ddebbff01f176
JeffMboya <jangina.mboya@gmail.com>
2024-07-19 12:08:14 +0300

    refactor: update domains variable
    Signed-off-by: JeffMboya <jangina.mboya@gmail.com>

--- docker/certbot/init-letsencrypt.sh
+++ docker/certbot/init-letsencrypt.sh
@@ -27,14 +27,13 @@ if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/
 fi
 
 echo "### Creating dummy certificate for $domains ..."
-path="/etc/letsencrypt/live/${domains[0]}"
-echo "Path: $path"
-mkdir -p "$data_path/conf/live/${domains[0]}"
+path="/etc/letsencrypt/live/${domains}"
+mkdir -p "$data_path/conf/live/${domains}"
 docker compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1\
     -keyout '$path/privkey.pem' \
     -out '$path/fullchain.pem' \
-    -subj '/CN=${domains[0]}'" certbot
+    -subj '/CN=${domains}'" certbot
 echo
 
 echo "### Starting nginx ..."
@@ -43,9 +42,9 @@ echo
 
 echo "### Deleting dummy certificate for $domains ..."
 docker compose run --rm --entrypoint "\
-  rm -Rf /etc/letsencrypt/live/${domains[0]} && \
-  rm -Rf /etc/letsencrypt/archive/${domains[0]} && \
-  rm -Rf /etc/letsencrypt/renewal/${domains[0]}.conf" certbot
+  rm -Rf /etc/letsencrypt/live/${domains} && \
+  rm -Rf /etc/letsencrypt/archive/${domains} && \
+  rm -Rf /etc/letsencrypt/renewal/${domains}.conf" certbot
 echo
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
